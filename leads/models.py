6a75f5aa1e57a5c2d19871a7b0d65c269dfef608
pad-awan <arnaudhub@yahoo.fr>
2021-04-23 00:19:56 +0200

    classes commit

---
+++ leads/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+class Lead(models.Model):
+    
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
