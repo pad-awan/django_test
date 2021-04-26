a29b9218780bd847e105fb93a4aa980fae423e3a
pad-awan <arnaudhub@yahoo.fr>
2021-04-26 13:53:40 +0200

    authentication

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILESURLS_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
@@ -132,3 +133,5 @@ STATIC_ROOT = "static_root"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL ='leads.User'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_REDIRECT_URL = '/leads'
--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,18 @@
+from django.core.mail import send_mail
+# from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views import generic
 from .models import Lead, Agent
-from .forms import LeadForm, LeadModelForm
+from .forms import LeadForm, LeadModelForm,CustomUserCreationForm
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+    def get_success_url(self):
+        return reverse("login")
+
 
 class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
@@ -44,6 +54,17 @@ class LeadCreateView(generic.CreateView):
     form_class = LeadModelForm
     def get_success_url(self):
         return reverse("leads:lead-list")
+
+    def form_valid(self,form):
+        # TODO send mail
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView,self).form_valid(form)
+
 
 def lead_create(request):
     form = LeadModelForm()
